54118d267e09d0a754e33afe52ba5ac39e281e57
Randy Ofosu <randy_ofosu@hotmail.com>
2020-01-14 12:34:37 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,6 @@
+<?php
+for ($vragen=0; $vragen < 1; $vragen++) {
+    $voornaam = readline("Wat is je voornaam?".PHP_EOL);
+    $achternaam = readline("Wat is je achternaam?".PHP_EOL);
+    echo "Jouw naam is: ${voornaam} ${achternaam}".PHP_EOL;
+}
